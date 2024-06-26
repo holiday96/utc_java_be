e570403eb8281039c47b2c7fadc4dc777bfe9f03
dat.ma <thedat1705k2@gmail.com>
2024-06-26 12:24:30 +0700

    convert config mysql to oracle 19c

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework:spring-context:5.3.23")
-	implementation("mysql:mysql-connector-java")
+	// https://mvnrepository.com/artifact/com.oracle.ojdbc/ojdbc8
+	implementation("com.oracle.ojdbc:ojdbc8:19.3.0.0")
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
